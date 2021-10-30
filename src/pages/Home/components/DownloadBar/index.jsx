c1764085b2cc45fd37011d670e41121475a3d3cd
Ben Schoelkopf <thebenschoel@gmail.com>
2021-10-30 13:55:38 -0400

    zips images

--- src/pages/Home/components/DownloadBar/index.jsx
+++ src/pages/Home/components/DownloadBar/index.jsx
@@ -1,3 +1,4 @@
+import JSZip from "jszip";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
@@ -52,9 +53,54 @@ const PrimaryButton = styled(Button)`
     color: #ffffff;
 `;
 
+const imageToBlob = (imageURL) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.crossOrigin = "Anonymous";
+
+        img.onload = () => {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+            canvas.height = img.naturalHeight;
+            canvas.width = img.naturalWidth;
+            ctx.drawImage(img, 0, 0);
+
+            canvas.toBlob(resolve);
+        };
+
+        img.src = imageURL;
+    });
+};
+
 const DownloadBar = () => {
     const selectedImages = useSelector((state) => state.pets.selectedPets);
     const dispatch = useDispatch();
+
+    const [isDownloading, setIsDownloading] = React.useState(false);
+
+    const downloadImages = async () => {
+        setIsDownloading(true);
+
+        Promise.all(selectedImages.map(imageToBlob)).then((results) => {
+            const zip = new JSZip();
+
+            for (let i = 0; i < results.length; i++) {
+                zip.file(`pet-${i + 1}.jpeg`, results[i]);
+            }
+
+            zip.generateAsync({ type: "blob" }).then((content) => {
+                const a = document.createElement("a");
+                a.href = URL.createObjectURL(content);
+                a.download = "pets.zip";
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+
+                dispatch(clearSelections());
+                setIsDownloading(false);
+            });
+        });
+    };
 
     return (
         <Wrapper visible={selectedImages.length !== 0}>
@@ -64,8 +110,12 @@ const DownloadBar = () => {
                     <Button style={{ marginRight: "0.5rem" }} onClick={() => dispatch(selectAll())}>
                         Select all
                     </Button>
-                    <PrimaryButton>
-                        Download {selectedImages.length} image{selectedImages.length > 1 ? "s" : ""}
+                    <PrimaryButton onClick={downloadImages}>
+                        {isDownloading
+                            ? "Downloading..."
+                            : `Download ${selectedImages.length} image${
+                                  selectedImages.length > 1 ? "s" : ""
+                              }`}
                     </PrimaryButton>
                 </div>
             </Content>
